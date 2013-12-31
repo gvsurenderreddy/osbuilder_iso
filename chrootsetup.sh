68c320e16064c3d00ade221652c5a8feb1a23089
varcorsoftware <tvarwig@varcorsoftware.com>
2013-12-31 14:14:37 -0600

    Update chrootsetup.sh

--- chrootsetup.sh
+++ chrootsetup.sh
@@ -51,8 +51,7 @@ apt-get upgrade --yes
 
 # Installing Programs
 printf "e[01;32mInstalling Chromium Browser\ne[00m"
-apt-get install --yes chromium-browser preload bleachbit
-
+apt-get install --yes chromium-browser preload bleachbit ufw apparmor apparmor-profiles psad
 
 rm /var/lib/dbus/machine-id
 
